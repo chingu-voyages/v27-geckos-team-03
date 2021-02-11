71969f0343f8339e26dd3d0b1ba0f44caf115439
sridevi2014 <sridevips@hotmail.com>
2021-02-10 22:37:34 -0600

    added Login from and signup form

---
+++ health-on-time/src/services/auth.service.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/api/auth/";
+
+const register = (username, email, phoneNumber, password) => {
+  return axios.post(API_URL + "signup", {
+    username,
+    email,
+    phoneNumber,
+    password,
+  });
+};
+
+const login = (email, password) => {
+  return axios
+    .post(API_URL + "signin", {
+      email,
+      password,
+    })
+    .then((response) => {
+      if (response.data.accessToken) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+      }
+
+      return response.data;
+    });
+};
+
+const logout = () => {
+  localStorage.removeItem("user");
+};
+
+const getCurrentUser = () => {
+  return JSON.parse(localStorage.getItem("user"));
+};
+
+export default {
+  register,
+  login,
+  logout,
+  getCurrentUser,
+};
