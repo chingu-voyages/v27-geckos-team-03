2bd074b508cd3a591be1be0209baadeee584f446
kapre <kapremom@gail.com>
2021-02-13 19:48:51 -0500

    footer now in components folder

--- health-on-time/src/index.js
+++ health-on-time/src/index.js
@@ -1,9 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Components/Footer.js';
+
 
 ReactDOM.render(
   <React.StrictMode>
