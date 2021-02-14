bb335b7aa01d72d67728b1e9b37d7be93d035b54
kapre <kapremom@gail.com>
2021-02-13 21:00:53 -0500

    Navbar now in Components Folder

--- health-on-time/src/App.js
+++ health-on-time/src/App.js
@@ -1,32 +1,11 @@
 import React from 'react';
 import "./App.css";
-import * as ReactBootStrap from "react-bootstrap";
+import Navbar from "./Components/Navbar.js";
 
 function App() {
   return (
     <div className="App">
-      <ReactBootStrap.Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
-  <ReactBootStrap.Navbar.Brand href="#home">Health on Time</ReactBootStrap.Navbar.Brand>
-  <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
-    <ReactBootStrap.Nav className="mr-auto">
-      <ReactBootStrap.NavDropdown title="Settings" id="collasible-nav-dropdown">
-        <ReactBootStrap.NavDropdown.Item href="#action/3.1">Profile Page</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item href="#action/3.2">Medicine Cabinet</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item href="#action/3.3">Calendar</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Divider />
-        <ReactBootStrap.NavDropdown.Item href="#action/3.4"></ReactBootStrap.NavDropdown.Item>
-      </ReactBootStrap.NavDropdown>
-    </ReactBootStrap.Nav>
-    <ReactBootStrap.Nav>
-      <ReactBootStrap.Nav.Link href="#home">Home </ReactBootStrap.Nav.Link>
-      <ReactBootStrap.Nav.Link href="#about">About</ReactBootStrap.Nav.Link>
-      <ReactBootStrap.Nav.Link eventKey={2} href="#signup">
-        Signup
-      </ReactBootStrap.Nav.Link>
-    </ReactBootStrap.Nav>
-  </ReactBootStrap.Navbar.Collapse>
-</ReactBootStrap.Navbar>
+       <Navbar/>
     </div>
   );
 }
