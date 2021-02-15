c4beb44a78e718f662439b0cdedfe59a6b6aac1c
sridevi2014 <sridevips@hotmail.com>
2021-02-15 12:23:45 -0600

    added Login from and signup form

--- health-on-time/src/App.js
+++ health-on-time/src/App.js
@@ -1,8 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+
 
 function App() {
-  return <div className="App">Health on Time</div>;
+  return (
+    <div className="container mt-3">
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
