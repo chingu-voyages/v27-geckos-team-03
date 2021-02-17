3de54979628ffea42b7d5a38a62e5a9a4dbb0d5a
chrisvsanchez <chrisvsanchez@gmail.com>
2021-02-17 10:09:46 -0500

    resolved navbar bug

--- health-on-time/src/App.js
+++ health-on-time/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import React from "react";
 import LandingPage from "./components/landingPage";
-
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <LandingPage />
     </div>
   );
