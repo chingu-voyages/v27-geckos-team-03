1edb814b18860a7a893f1afefa873cad1c48f760
chrisvsanchez <chrisvsanchez@gmail.com>
2021-02-17 12:24:10 -0500

    error resolved with src/index.js

--- health-on-time/src/index.js
+++ health-on-time/src/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-<<<<<<< HEAD
-import LandingPage from "./components/landingPage";
-=======
 import App from "./App";
->>>>>>> Development
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 
