aa82f24e2bdb282729f953693f7f0d0a85a33f45
sridevi2014 <sridevips@hotmail.com>
2021-02-22 22:32:57 -0600

    deleted conflicts

--- health-on-time/src/App.js
+++ health-on-time/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import {BrowserRouter, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Profile from "./components/Profile";
@@ -9,13 +9,11 @@ import Profile from "./components/Profile";
 function App() {
   return (
     <div className="container mt-3">
-      <BrowserRouter>
       <Switch>
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/profile" component={Profile} />
       </Switch>
-      </BrowserRouter>
     </div>
   );
 }
