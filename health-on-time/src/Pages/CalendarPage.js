0b0c674576bdb346ee42c91c9d1daa98cb8e58ef
tofujlg <tofuredbull@gmail.com>
2021-02-27 22:14:18 +0100

    Initial commit

--- health-on-time/src/Pages/CalendarPage.js
+++ health-on-time/src/Pages/CalendarPage.js
@@ -4,6 +4,25 @@ const CalendarPage = () => {
   return (
     <div>
       <h1>Calendar</h1>
+      <div>
+        <h2>Monday 1st March</h2>
+        <h4>Medicine Name</h4>
+        <p>Time: 2 pills in the morning</p>
+        <button>Complete</button>
+        <p>Time: 2 pills in the morning</p>
+        <button>Complete</button>
+
+      </div>
+      <div>
+      <h2>Tuesday 2nd March</h2>
+      <h4>Medicine Name</h4>
+      <p>Time: 2 pills in the morning</p>
+      <button>Complete</button>
+
+      <p>Time: 2 pills in the morning</p>
+      <button>Complete</button>
+
+    </div>
     </div>
   );
 };
