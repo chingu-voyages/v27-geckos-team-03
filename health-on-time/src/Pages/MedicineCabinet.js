a7ce70beaa58b444eaf6f44280b63cf7b31c974b
Sridevi Patchipulusu <sridevips@hotmail.com>
2021-03-05 15:47:33 -0600

    delete medication

--- health-on-time/src/Pages/MedicineCabinet.js
+++ health-on-time/src/Pages/MedicineCabinet.js
@@ -1,9 +1,34 @@
 import React from "react";
 import SearchMedication from "../Components/SearchMedication";
+import { Button, Card, CardDeck } from "react-bootstrap";
 
-const MedicineCabinet = () => {
+
+const MedicineCabinet = ({medications}) => {
+
+  let deleteMedication = (e) => {
+    console.log(e.target.value);
+  };
+
   return (
     <div>
+      {console.log(medications)} 
+      <h2>Current User medications</h2>
+      {medications.map((med) => (
+          <div key={med.id}>
+          <h3>{med.name}</h3>
+          <h3>{med.description}</h3>
+          <button onClick={deleteMedication}>delete</button>
+        </div>
+      ))} 
+    
+          <Card key={med.id}>
+            <Card.Body>
+              <Card.Title>{med.Name}</Card.Title>
+              <Card.Text>Description {med.Description}</Card.Text>
+              <Button>Delete</Button>
+            </Card.Body>
+          </Card>
+
       <h1>MedicineCabinet</h1>
       <SearchMedication />
     </div>
