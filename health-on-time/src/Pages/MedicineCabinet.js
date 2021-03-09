917ef4b0ddafc620d53a0c1d3a23d8ef558678b1
chrisvsanchez <chrisvsanchez@gmail.com>
2021-03-08 21:40:39 -0500

    commented out card in medicine cabinet

--- health-on-time/src/Pages/MedicineCabinet.js
+++ health-on-time/src/Pages/MedicineCabinet.js
@@ -2,10 +2,8 @@ import React from "react";
 import SearchMedication from "../Components/SearchMedication";
 import { Button, Card, CardDeck } from "react-bootstrap";
 
-
-const MedicineCabinet = ({medications}) => {
-
-/*   turnToCards.map((med) => {
+const MedicineCabinet = ({ medications }) => {
+  /*   turnToCards.map((med) => {
     return medications.map((med) => (
         <Card key={med.id}>
             <Card.Body>
@@ -18,28 +16,28 @@ const MedicineCabinet = ({medications}) => {
  */
   let deleteMedication = (e) => {
     console.log(e.target.value);
-/* {turnToCards()} */
+    /* {turnToCards()} */
   };
 
   return (
     <div>
-      {console.log(medications)} 
+      {console.log(medications)}
       <h2>Current User medications</h2>
       {medications.map((med) => (
-          <div key={med.id}>
+        <div key={med.id}>
           <h3>{med.name}</h3>
           <h3>{med.description}</h3>
           <button onClick={deleteMedication}>delete</button>
         </div>
-      ))} 
-    
-          <Card key={med.id}>
+      ))}
+
+      {/* <Card key={med.id}>
             <Card.Body>
               <Card.Title>{med.Name}</Card.Title>
               <Card.Text>Description {med.Description}</Card.Text>
               <Button>Delete</Button>
             </Card.Body>
-          </Card>
+          </Card> */}
 
       <h1>MedicineCabinet</h1>
       <SearchMedication />
