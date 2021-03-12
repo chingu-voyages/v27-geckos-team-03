8bfb3d9ba1c0a751f8d07c8695a44e8cc7b4ff06
Lewis Shepherd <71042305+shepherdlewis000@users.noreply.github.com>
2021-03-12 12:53:54 -0700

    3-12 noonish

--- health-on-time/src/Pages/AddMedication.js
+++ health-on-time/src/Pages/AddMedication.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SearchMedication from '../Components/SearchMedication';
 import { Container } from 'react-bootstrap';
 
@@ -7,16 +7,26 @@ import { Container } from 'react-bootstrap';
 const AddMedication = () => {
   
   const [chosenMed, setChosenMed] = useState();
-  
+  useEffect(() => {
+    if (chosenMed) {
+      console.log("11: ");
+      console.dir(chosenMed);
+    }
+  }, [chosenMed])
+
   return (
     <div>
       <Container>
         <h1>Add Medications</h1>
         <SearchMedication setChosenMed={setChosenMed} />
-        {
-          chosenMed ? chosenMed.name : null
+        
+        { /* console.log("19 addMedication.js: " +  chosenMed.name) */}
+        {chosenMed ? (
+          < h3 style={{ marginBottom: "200px" }}>{chosenMed.brandName}</h3>)
+          
+          :
+          null
         }
-
       </Container>
 
     </div>
